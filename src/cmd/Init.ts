52fb3d0d441b36758acb81aae2780e8fe463f699
hbtb <bishehsary@gmail.com>
2016-08-24 23:25:43 -0700

    updating README & help

--- src/cmd/Init.ts
+++ src/cmd/Init.ts
@@ -24,7 +24,7 @@ export class Init {
     }
 
     static parse(args: Array<string>) {
-        if (!args.length || ['-h', '--help', 'help'].indexOf(args[0]) >= 0) {
+        if (['-h', '--help', 'help'].indexOf(args[0]) >= 0) {
             return Init.help();
         }
         if (args.indexOf('--docker-compose') >= 0) return DockerUtil.installCompose();
--- src/cmd/Update.ts
+++ src/cmd/Update.ts
@@ -3,7 +3,7 @@ import {Vesta} from "../gen/file/Vesta";
 export class Update {
 
     static parse(args: Array<string>) {
-        if (!args.length || ['-h', '--help', 'help'].indexOf(args[0]) >= 0) {
+        if (['-h', '--help', 'help'].indexOf(args[0]) >= 0) {
             return Update.help();
         }
         Vesta.updatePackages(args);
@@ -18,7 +18,6 @@ Updates a package to it's latest version
 Options:
     --all   Update all npm packages (dependencies)
     --dev   Update all npm packages (devDependencies)
-    --tsd   Generates a sass file for a page (only sass type)
     
 Without any options, it will only update all vesta packages
 `);
